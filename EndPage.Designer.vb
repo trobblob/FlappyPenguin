d05fbac7d91784d996a0b5e0610cbdf2ee05896c
trobblob <48190250+trobblob@users.noreply.github.com>
2020-11-02 16:38:50 -0600

    Made penguin backgrounds transparent

--- EndPage.Designer.vb
+++ EndPage.Designer.vb
@@ -22,9 +22,16 @@ Partial Class EndPage
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.SuspendLayout()
+        '
+        'EndPage
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(673, 450)
+        Me.Name = "EndPage"
         Me.Text = "EndPage"
+        Me.ResumeLayout(False)
+
     End Sub
 End Class
