eca3a0ec629d91f7a5af5b52aca5e7cdde499f95
trobblob <tkirbyrob@gmail.com>
2020-11-12 18:49:08 -0600

    Fixed issue where dead penguin sprite was used in the game

--- EndPage.vb
+++ EndPage.vb
@@ -1,11 +1,19 @@
 ﻿Public Class EndPage
+
+    Dim pbxPenguin As Image
+
+    Sub New(penguinImage As Image)
+        ' This call is required by the designer.
+        InitializeComponent()
+        pbxPenguin = penguinImage
+    End Sub
+
     Private Sub btnBack_Click(sender As Object, e As EventArgs) Handles btnBack.Click
-        Me.Close()
+        Close()
     End Sub
 
     Private Sub btnReplay_Click(sender As Object, e As EventArgs) Handles btnReplay.Click
-
-        Dim frmFlappyPenguinForm As New FlappyPenguinForm(pbxFailedPenguin.Image)
+        Dim frmFlappyPenguinForm As New FlappyPenguinForm(pbxPenguin)
         frmFlappyPenguinForm.ShowDialog()
     End Sub
 End Class
